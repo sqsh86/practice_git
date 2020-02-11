f5c5df8d66d07dc89f998254564dfdabe36bc420
sqsh86 <sqsh86@sina.com>
2020-02-11 21:50:19 +0800

    second import.

--- git_test.py
+++ git_test.py
@@ -5,5 +5,10 @@ import numpy as np
 def hello_world():
     print 'Hello World'
 
+#this is second input.
+def hello_git():
+    print 'Hello git.'
+
 if __name__ == '__main__':
     hello_world()
+    hello_git()
